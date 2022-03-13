9a81f8da629772086fe6ea5206d587bd2839d54e
ThomasP850 <66078083+ThomasP850@users.noreply.github.com>
2022-03-13 14:13:01 -0400

    motor ports

--- src/main/java/frc/robot/subsystems/intake/BallIntake.java
+++ src/main/java/frc/robot/subsystems/intake/BallIntake.java
@@ -6,15 +6,23 @@ package frc.robot.subsystems.intake;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class BallIntake extends SubsystemBase {
+  private static final int INTAKE_MOTOR_CAN = 30;
+  private static final int PCM_CAN = 2;
+  private static final int 
+
   private TalonSRX intakeMotor;
+
+  private DoubleSolenoid leftSolenoid;
+  private DoubleSolenoid rightSolenoid;
 
 
   /** Creates a new BallIntake. */
   public BallIntake(int intakeMotorChannel) {
-
+    intakeMotor = new TalonSRX(30)
   }
 
   @Override
