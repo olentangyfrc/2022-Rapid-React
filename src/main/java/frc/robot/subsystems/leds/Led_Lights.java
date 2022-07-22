b283ac29b09e37e0688c6935ed504eebacad59fe
ThomasP850 <66078083+ThomasP850@users.noreply.github.com>
2022-07-22 13:50:58 -0400

    clean up LED subsystem

--- src/main/java/frc/robot/subsystems/leds/Led_Lights.java
+++ src/main/java/frc/robot/subsystems/leds/Led_Lights.java
@@ -1,17 +1,18 @@
 package frc.robot.subsystems.leds;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+/**
+ * This subsystem controls the decorative LED lights around the shooter wheel of the robot
+ */
 public class Led_Lights extends SubsystemBase {
 
+  // Used to send PWM signals to the LEDs
+  private static Spark m_blinkin;
 
-  private static Spark m_blinkin = null;
-
+  // Color codes
   public static final double RED_DEFAULT_CODE = -0.85;
   public static final double BLUE_DEFAULT_CODE = -0.83;
   public static final double WHITE_DEFAULT_CODE = -0.81;
@@ -24,13 +25,10 @@ public class Led_Lights extends SubsystemBase {
   
   public Led_Lights(int pwmPort) {
     m_blinkin = new Spark(pwmPort);
-    SmartDashboard.putBoolean("Initalized lights", true);
-
   }
 
   @Override
   public void periodic() {
-    
     switch(DriverStation.getAlliance()) {
       case Red:
         if(isShooting) {
@@ -58,13 +56,22 @@ public class Led_Lights extends SubsystemBase {
     }
   }
 
- 
+  /**
+   * Set the given color code to the LEDs.
+   * 
+   * @param val The color code to set the LEDs to. If this isn't in the range [-1, 1] it will not be set.
+   */
   public void set(double val) {
     if ((val >= -1.0) && (val <= 1.0)) {
       m_blinkin.set(val);
     }
   }
 
+  /**
+   * Tell the LED subsystem whether or not the robot is shooting
+   * 
+   * @param isShooting True if the robot is shooting, otherwise, false
+   */
   public void setIsShooting(boolean isShooting) {
     this.isShooting = isShooting;
   }
